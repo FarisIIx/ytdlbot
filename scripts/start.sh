19213afe15ed0fc1c93c6bed4584041f2f1b18d4
FarisIIx <67123402+FarisIIx@users.noreply.github.com>
2023-05-03 18:29:47 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 docker run -d --restart unless-stopped --name ytdl \
    --net host \
-     -e TOKEN=12345 \
-     -e APP_ID=123123 \
-     -e APP_HASH=4990 \
+     -e TOKEN=6138041049:AAETY4JmrA1k77OISyB4UcYjmnCmoQpVIuU \
+     -e APP_ID=24880453 \
+     -e APP_HASH=28b19ba814d9b7a680e89e7a98fd681c \
      -e ENABLE_CELERY=True \
      -e REDIS=192.168.6.1 \
      -e MYSQL_HOST=192.168.6.1 \
